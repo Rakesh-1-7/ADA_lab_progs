d28a8ae8f112dad4bdf02baaa9c944ebd6261427
Rakesh B N <68104336+Rakesh-1-7@users.noreply.github.com>
2022-07-19 22:31:16 +0530

    insertion_sort.c

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,29 +1,32 @@
-#include<stdio.h>
-#include<string.h>
-void inserion_sort(int a[],int n)
+#include <stdio.h>
+#include <string.h>
+void inserion_sort(int a[], int n)
 {
-    int y,j;
-    for(int i = 1;i < n;i++)
+    int y, j;
+    for (int i = 1; i < n; i++)
     {
         y = a[i];
-        j=i-1;
-        while(j >= 0 && a[j] > y)
+        j = i - 1;
+        while (j >= 0 && a[j] > y)
         {
-            a[j+1] = a[j];
-            j-=1;
+            a[j + 1] = a[j];
+            j -= 1;
         }
-        a[j+1] = y;
+        a[j + 1] = y;
     }
 }
 
 int main()
 {
-    int a[] = {1,10,4,2,5,-6};
-    inserion_sort(a,strlen(a));
+    int a[10], n;
+    printf("Enter the value of n : ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    inserion_sort(a, n);
     printf("Start");
-    for(int i = 0; i<strlen(a);i++)
-        printf(" -> %d",a[i]);
+    for (int i = 0; i < n; i++)
+        printf(" -> %d", a[i]);
 
     return 0;
-
 }
