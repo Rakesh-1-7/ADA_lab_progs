bb08b48c7c219922db7e718b4e2f17e7d062b994
Rakesh B N <68104336+Rakesh-1-7@users.noreply.github.com>
2022-07-29 21:21:04 +0530

    Update merge_sort.c

--- merge_sort.c
+++ merge_sort.c
@@ -1,30 +1,29 @@
-#include<stdio.h>
-#include<time.h>
-#include<conio.h>
+#include <stdio.h>
+#include <time.h>
+#include <conio.h>
+#include <stdlib.h>
 
 void merge(int a[], int low, int mid, int high)
 {
-    int i, j, k,c[100];
+    int i, j, k, c[100];
     i = low;
     k = low;
-    j = mid+1;
-    for(int x = 0; x<10000; x++);
+    j = mid + 1;
+
+    for (int x = 0; x < 10000; x++);
     while (i <= mid && j <= high)
     {
-        if (a[i] < a[j])
+        if (a[i] <= a[j])
             c[k++] = a[i++];
-        else
+        if (a[i] > a[j])
             c[k++] = a[j++];
     }
     while (i <= mid)
         c[k++] = a[i++];
-
     while (j <= high)
         c[k++] = a[j++];
-
     for (i = low; i <= high; i++)
         a[i] = c[i];
-
 }
 
 void mergesort(int a[], int low, int high)
@@ -38,31 +37,26 @@ void mergesort(int a[], int low, int high)
         merge(a, low, mid, high);
     }
 }
-
-
-
 int main()
 {
-    int i,n,a[100];
+    int i, n, a[100];
     clock_t start, end;
     printf("Enter number of elements : ");
-    scanf("%d",&n);
-    printf("Enter the array elements : ");
-    for(i=0;i<n;i++)
+    scanf("%d", &n);
+    printf("The array elements are : ");
+    for (i = 0; i < n; i++)
     {
-        scanf("%d",&a[i]);
+        a[i] = rand() % 100;
+        printf("%d ", a[i]);
     }
-
     start = clock();
-    mergesort(a,0,n-1);
+    mergesort(a, 0, n - 1);
     end = clock();
-
     printf("The sorted array is : ");
-    for(i=0;i<n;i++)
+    for (i = 0; i < n; i++)
     {
-        printf("%d ",a[i]);
+        printf("%d ", a[i]);
     }
-
-    printf("\nTime taken : %f",(double)(end-start)/CLOCKS_PER_SEC);
+    printf("\nTime taken : %f", (double)(end - start) / CLOCKS_PER_SEC);
     return 0;
 }
