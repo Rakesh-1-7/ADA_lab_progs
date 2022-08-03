0f4f3280c039a98113c04f7e3f64e43a5e4f7490
Rakesh B N <68104336+Rakesh-1-7@users.noreply.github.com>
2022-08-03 15:44:26 +0530

    Update knapsack.c

--- knapsack.c
+++ knapsack.c
@@ -1,79 +1,59 @@
 #include<stdio.h>
+#include<conio.h>
+int w[10],p[10],v[10][10],n,i,j,cap,x[10]= {0};
 
-int knapsack(int, int);
-void object_selected(int , int);
-int max(int , int);
-int W[10], P[10], V[10][10], X[10], i, j;
-int max(int x, int y)
+int max(int i,int j) 
 {
-    if(x>y)
-        return x;
-    else
-        return y;
+	return ((i>j)?i:j);
 }
-
-int main()
+int knap(int i,int j) 
 {
-    int n, m, solution;
-    printf("Enter number of objects : ");
-    scanf("%d", &n);
-
-    printf("Enter capacity of Knapsack : ");
-    scanf("%d", &m);
-
-    printf("Enter objects weight : ");
-    for(i = 1; i <= n; i++)
-        scanf("%d",&W[i]);
-
-    printf("Enter profit of objects : ");
-    for(i = 1; i <= n; i++)
-        scanf("%d", &P[i]);
-
-    solution = knapsack(n,m);
-
-    printf("Optimal solution = %d", solution);
-
-    object_selected(n,m);
-    // getch();
-    return 0;
-    
+	int value;
+	if(v[i][j]<0) {
+		if(j<w[i])
+		   value=knap(i-1,j); else
+		   value=max(knap(i-1,j),p[i]+knap(i-1,j-w[i]));
+		v[i][j]=value;
+	}
+	return(v[i][j]);
 }
-
-int knapsack(int n, int m)
+void main() 
 {
-    for(i = 1; i <= n; i++)
+	int profit,count=0;
+	// clrscr();
+	printf("\nEnter the number of elements\n");
+	scanf("%d",&n);
+	printf("Enter the profit and weights of the elements\n");
+	for (i=1;i<=n;i++) 
     {
-        for(j = 0; j <= n; j++)
-        {
-            if(i == 0 || j == 0)
-                V[i][j] = 0;
-            else if(W[i] > j)
-                V[i][j] = V[i-1][j];
-            else
-                V[i][j] = max(V[i-1][j],V[i-1][j-W[i]]+P[i]);
-        }
-    }
-    return V[n][m];
-}
+		printf("For item no %d : ",i);
+		scanf("%d%d",&p[i],&w[i]);
+	}
+	printf("\nEnter the capacity \n");
+	scanf("%d",&cap);
+	for (i=0;i<=n;i++)
+	  for (j=0;j<=cap;j++)
+	   if((i==0)||(j==0))
+	    v[i][j]=0; else
+	    v[i][j]=-1;
+	profit=knap(n,cap);
+	i=n;
+	j=cap;
+	while(j!=0&&i!=0) 
+    {
+		if(v[i][j]!=v[i-1][j]) {
 
-void object_selected(int n, int m)
-{
-    i = n;
-    j = m;
-    while(i != 0 && j != 0)
-    {
-        if(V[i][j] != V[i-1][j])
-        {
-            X[i] = 1;
-        }
-        j = j - W[i];
-        i--;
-    }
-
-    printf("Object selected : ");
-    for(i = 1; i <= n; i++)
-    {
-        if(X[i] == 1)
-        printf("%d", i);
-    }
+			x[i]=1;
+			j=j-w[i];
+			i--;
+		} else
+		   i--;
+	}
+	printf("Items included are\n");
+	printf("Sl.no\tweight\tprofit\n");
+	for (i=1;i<=n;i++)
+	  if(x[i])
+	   printf("%d\t%d\t%d\n",++count,w[i],p[i]);
+	printf("Total profit = %d\n",profit);
+	getch();
 }
